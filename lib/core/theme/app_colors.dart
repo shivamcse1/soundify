6a73d253f8f4e232febf569b27494e2dbe72226e
shivamcse1 <shiv.cse1@gmail.com>
2025-09-02 13:19:00 +0530

    start music player

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme Colors
+  static const Color lightPrimary = Color(0xFF6C63FF); // Deep Purple
+  static const Color lightSecondary = Color(0xFFFF6F91); // Pink Accent
+  static const Color lightBackground = Color(0xFFF9F9FB); // Soft White
+  static const Color lightSurface = Color(0xFFFFFFFF); // White Card
+  static const Color lightTextPrimary = Color(0xFF1C1C1C); // Black
+  static const Color lightTextSecondary = Color(0xFF555555); // Gray
+
+  // Common color //
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF1C1C1C);
+  static const Color grey = Color(0xFF1E1E1E);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color(0xFF4FC3F7); // Electric Blue
+  static const Color darkSecondary = Color(0xFFFF4081); // Neon Pink
+  static const Color darkBackground = Color(0xFF121212); // Charcoal
+  static const Color darkSurface = Color(0xFF1E1E1E); // Dark Card
+  static const Color darkTextPrimary = Color(0xFFFFFFFF); // White
+  static const Color darkTextSecondary = Color(0xFFB3B3B3); // Grayish
+}
